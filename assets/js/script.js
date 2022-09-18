a6200970ae5399ccd47d97d85af802c83d64fcb1
amaragh <mercer.alecia@gmail.com>
2022-09-17 22:17:30 -0500

    build function to extract weather for current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city");
 
+var cityNameEl = document.querySelector("#city-name");
+var currentDateEl =  document.querySelector("#current-date");
+var weatherIconEl = document.querySelector(".weather-icon");
+
+var currentTempEl = document.querySelector("#current-temp");
+var currentHumidEl = document.querySelector("#current-humid");
+var currentWindEl = document.querySelector("#current-wind");
+
+
 // function to get city coordinates
 var getCityCoords = function (city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=718908fbd7f74c0aac0bd0ff69325a23";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=718908fbd7f74c0aac0bd0ff69325a23&units=metric";
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                
+                console.log(data);
                 var latitude = data.coord.lat;
                 var longitude = data.coord.lon;
                 // function to extract current day weather
                 getCurrentWeather(data);
                 // function to call other api with the coordinates as query parameters
                 getForecast(latitude, longitude);
-                console.log(data);
+
             })
         } else {
             alert("Error: City Not Found. Please enter a valid city name.")
@@ -30,6 +39,7 @@ var formSubmitHandler = function (event) {
 
     if (cityName) {
         // call get function to retrieve city coordinates and today's weather
+        getCityCoords(cityName);
         cityInputEl.value = "";
     } else {
         alert("Please enter a city name");
@@ -38,11 +48,32 @@ var formSubmitHandler = function (event) {
 }
 
 var getCurrentWeather = function (data) {
+    console.log("get current weather");
+
+    var city = data.name;
+    var weather = data.weather[0].id;
+    var temperature = data.main.temp;
+    var humidity = data.main.humidity;
+    var windSpeed = data.wind.speed;
+
+    // convert unix timecode to today's date in US format
+    var date = new Date(data.dt * 1000).toLocaleDateString("en-US");
+
+    cityNameEl.textContent = city;
+    currentDateEl.textContent = date;
+    weatherIconEl.setAttribute("href", "./assets/images/800.png");
+
+    currentTempEl. textContent = "Temp = " + temperature + "°F";
+    currentWindEl.textContent = "Wind = " + windSpeed + " MPH";
+    currentHumidEl. textContent = "Humidity = " + humidity + "%";
+
+    console.log(city, weather, temperature, humidity, windSpeed);
+
 
 };
 
-var getForecast = function(latitude, longitude) {
-
+var getForecast = function (latitude, longitude) {
+    console.log("latitude", latitude, "longitude", longitude);
 }
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
